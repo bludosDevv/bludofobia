1b405c9842c78c80b96a66a9e1d6c089cd6b2c16
BludosDev <bludosofficial+67@gmail.com>
2026-01-28 23:49:04 +0530

    Initial commit

---
+++ src/main/java/com/bludosdev/fobia/Main.java
@@ -0,0 +1,10 @@
+package com.bludosdev.fobia;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("bludofobia")
+public class Main {
+    public Main() {
+        System.out.println("Hello from bludofobia!");
+    }
+}
